0f81e2b009a09d6b921ea8bded5afdce66010355
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-11-03 21:51:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ class Polygon {
 }
 
 class Triangle extends Polygon {
+
+  isGreater(array) {
+    return this.sides[i] + this.sides[i]) > this.sides[i]
+  }
   get isValid(){
     return ((this.sides[0] + this.sides[1]) > this.sides[2]) &&
     ((this.sides[1] + this.sides[2]) > this.sides[0]) &&
