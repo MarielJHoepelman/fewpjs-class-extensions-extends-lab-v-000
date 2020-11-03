01929ad144b389b5c28d615ada08000cd194883b
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-11-03 21:14:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ class Polygon {
 class Triangle extends Polygon {
   isValid(){
     // The sum of the lengths of any two sides of a triangle is greater than the length of the third side.
-    return (this.sides[0] + this.sides[1] > this.sides[2]) ||  
-    (this.sides[1] + this.sides[2] > this.sides[0]) ||  
+    return (this.sides[0] + this.sides[1] > this.sides[2]) ||
+    (this.sides[1] + this.sides[2] > this.sides[0]) ||
     (this.sides[0] + this.sides[2] > this.sides[1])
   }
 }
